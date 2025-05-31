a7ce89bbbe528ebd1ba6b74c51c0223d08ce1eb7
Ouissale <117834480+ouissale-ait-aissa@users.noreply.github.com>
2025-05-31 12:18:25 +0200

    Update log-analyzer.py

--- log-analyzer.py
+++ log-analyzer.py
@@ -20,3 +20,19 @@ with open("rapport.txt", "w") as report:
 print(f"{Fore.RED}ERROR: {stats['ERROR']}")
 print(f"{Fore.YELLOW}WARNING: {stats['WARNING']}")
 print(f"{Fore.GREEN}INFO: {stats['INFO']}")
+
+import sys
+
+error_count = 0
+with open('log.txt', 'r') as f:
+    for line in f:
+        if "ERROR" in line:
+            error_count += 1
+
+if error_count > 5:
+    print(f"Erreur critique : {error_count} erreurs détectées. Build échoue.")
+    sys.exit(1)  # Jenkins détectera l’échec
+else:
+    print(f"Nombre d’erreurs OK : {error_count}")
+    sys.exit(0)  # Build réussi
+
